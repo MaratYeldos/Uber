97a072b39cf2561d84827bd22f89f3ac5a63e5e7
Yeldos <eldos.boom001@gmail.com>
2022-10-28 14:41:06 +0600

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Yeldos Marat on 28.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
