703d770a2efbf3dec17234f12354dd9766e0ff0e
Yeldos <eldos.boom001@gmail.com>
2022-10-30 13:21:25 +0600

    feat: mapview state managment

---
+++ UberClone/Models/MapViewState.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewState.swift
+//  UberClone
+//
+//  Created by Yeldos Marat on 29.10.2022.
+//
+
+import Foundation
+
+enum MapViewState {
+    case noInput
+    case searchingForLocation
+    case locationSelected
+}
